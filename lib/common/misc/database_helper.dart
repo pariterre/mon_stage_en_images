5af723d34b66b73f252a85db6be6fc6c61696459
Pariterre <pariterre@hotmail.com>
2026-01-07 16:09:06 -0500

    Fixed bug where changing in valid date is on january instead of july

--- lib/common/misc/database_helper.dart
+++ lib/common/misc/database_helper.dart
@@ -12,5 +12,7 @@ String pathToEmail(String reducedEmail) {
 
 // We need this because before septembre 2025, this field did not exist
 final defaultCreationDate = DateTime(2024, 9, 1).toIso8601String();
+final _today = DateTime.now();
 final DateTime isActiveLimitDate =
-    DateTime(DateTime.now().year).add(const Duration(days: 30 * 7));
+    DateTime(_today.month < 7 ? _today.year - 1 : _today.year)
+        .add(const Duration(days: 30 * 7));
